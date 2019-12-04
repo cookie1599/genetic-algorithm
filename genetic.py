b1912c389415d2a546e0f76a149e9bd098a789b2
Atrisa Endya N H <atrisaendya@gmail.com>
2019-12-04 23:16:01 +0700

    add

---
+++ genetic.py
@@ -0,0 +1,54 @@
+geneSet="abcdefghijklmnopqrstuvwxyz1234567890-"
+target="hazmo0on" #you can 
+
+
+import random
+import datetime
+
+#generate a guess
+def generate_parent (length) :
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampleSize))
+    return''.join(genes)
+
+#fitness
+def get_fitness(guess):
+    return sum(1 for expected, actual in zip(target, guess) 
+    if expected == actual)
+
+
+#mutate
+def mutate(parent):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet, 2)
+    childGenes[index] = alternate \
+        if newGene == childGenes[index] \
+        else newGene
+    return ''.join(childGenes)
+
+#Display
+def display(guess):
+    timeDiff = datetime.datetime.now() - startTime
+    fitness = get_fitness(guess)
+    print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
+
+#main
+random.seed()
+startTime = datetime.datetime.now()
+bestParent = generate_parent(len(target))
+bestFitness = get_fitness(bestParent)
+display(bestParent)
+
+while True:
+    child = mutate(bestParent)
+    childFitness = get_fitness(child)
+    if bestFitness >= childFitness:
+        continue
+    display(child)
+    if childFitness >= len(bestParent):
+        break
+    bestFitness = childFitness
+    bestParent = child
